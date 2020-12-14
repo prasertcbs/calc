b8412d0864c0131634057c5357ba62d5df975165
prasertcbs <bluecotton.k@gmail.com>
2020-12-14 21:14:42 +0700

    add docstring

--- length.py
+++ length.py
@@ -2,4 +2,12 @@ def inch2cm(inch):
     return inch * 2.54
 
 def cm2inch(cm):
+    """cm to inch
+
+    Args:
+        cm ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return cm / 2.54
