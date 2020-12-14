c470eed0375a0604b2548fe5ea77fae7b1c9ef34
prasertcbs <bluecotton.k@gmail.com>
2020-12-14 20:50:01 +0700

    add docstring

--- temperature.py
+++ temperature.py
@@ -1,7 +1,15 @@
-def c2f(c):
+def c2f(c: float)->float:
     return c/5*9+32
 
-def f2c(f):
+def f2c(f: float)->float:
+    """convert Fahrenheit to Celsius
+
+    Args:
+        f (float): degree in Fahrenheit
+
+    Returns:
+        [type]: [description]
+    """    
     return (f-32)*5/9
 
 if __name__ == "__main__":
