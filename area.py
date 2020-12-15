7b0c3cfefb33d1f1469e5eb2a2dc7649ffd48c74
prasertcbs <bluecotton.k@gmail.com>
2020-12-15 07:27:14 +0700

    add docstring

--- area.py
+++ area.py
@@ -1,5 +1,19 @@
+import math
+
 def rectangle(w, h):
+    """[summary]
+
+    Args:
+        w ([type]): [description]
+        h ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return w * h
 
 def triangle(b, h):
     return .5 * b * h     
+
+def circle(r):
+    return math.pi * r * r
