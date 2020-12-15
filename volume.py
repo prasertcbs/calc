e0728572e0a50f39b63d1b8640fe873b55a9625b
prasertcbs <bluecotton.k@gmail.com>
2020-12-15 07:16:03 +0700

    add docstring to volume.py

--- volume.py
+++ volume.py
@@ -1,8 +1,25 @@
 import math
 
 def cubic(side):
+    """[summary]
+
+    Args:
+        side ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return side ** 3
 
 def cylinder(r, h):
+    """[summary]
+
+    Args:
+        r ([type]): [description]
+        h ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return math.pi * r * r * h
     
